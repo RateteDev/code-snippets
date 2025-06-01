c0da7b1fbff892e521a372d1f6c5e3152016fe1a
Ratete <105982649+RateteDev@users.noreply.github.com>
2025-06-01 20:08:26 +0900

    Create ubuntu-setup.bash

---
+++ ubuntu-setup.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# パッケージリストの更新とパッケージのアップデート
+sudo apt update -y && sudo apt upgrade -y
+
+# 開発系
+sudo apt install -y git gh unzip tree build-essential ffmpeg golang-go
+curl -fsSL https://bun.sh/install | bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+curl -fsSL https://get.docker.com | sh
+sudo snap install ngrok
+
+# node(nvm) <https://nodejs.org/ja/download>
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.bashrc
+nvm install node && nvm alias default node
+
+# directory setup
+mkdir -p ~/workspace/repo
+touch ~/workspace/.env
+
+# write .bashrc
+cat >> "$HOME/.bashrc" << 'EOF'
+# Load custom workspace bash settings
+if [ -f "$HOME/workspace/.custom_bashrc" ]; then
+    source "$HOME/workspace/.custom_bashrc"
+fi
+EOF
+# create .custom_bashrc
+cat > "$HOME/workspace/.custom_bashrc" << 'EOF'
+# move repo directory
+if [[ -z "$TERM_PROGRAM" || "$TERM_PROGRAM" != "vscode" ]]; then
+  cd /home/user/workspace/repo
+fi
+
+# load ~/workspace/.env
+if [ -f "$HOME/workspace/.env" ]; then
+  set -a
+  source "$HOME/workspace/.env"
+  set +a
+fi
+EOF
